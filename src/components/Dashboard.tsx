c3eab3387c0e36949a8590f399019147d003f14c
Brandon Eleuterio <brandoneleuterio@hotmail.com>
2021-12-21 11:47:36 -0700

    Showing total miles run on dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { useHomeFetch } from '../hooks/useHomeFetch';
+import Spinner from './Spinner';
 
 const Dashboard: React.FC = () => {
-  return <>Hello Dashboard</>;
+  const { state, loading, error } = useHomeFetch();
+  console.log(state);
+
+  if (error) return <div>Something went wrong...</div>;
+
+  let totalMilesRun: number = 0;
+  state.results.forEach((activity) => {
+    if (activity.type.toLowerCase() === 'run') {
+      totalMilesRun += activity.distance;
+    }
+  });
+
+  return (
+    <>
+      <h2>Total Distance Run</h2>
+      <p>{`${(totalMilesRun / 1000).toFixed(0)} km`}</p>
+      {loading && <Spinner />}
+    </>
+  );
 };
 
 export default Dashboard;
